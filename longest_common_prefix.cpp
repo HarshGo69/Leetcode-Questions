679a98bdc1543e401fe97979ab5e69e6c0451d96
Harsh Gosai <harshgosai23m@gmail.com>
2025-08-02 10:14:48 +0530

    Solved Longest Common Prefix Problem

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,20 @@
+//Longest Common Prefix 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution{
+    public:
+    string longestCommonPrefix( vector<string>& strs){
+        string result = "";
+        sort(strs.begin(),strs.end());
+        int n = strs.size();
+        string first=strs[0],last=strs[n-1];
+        for(int i=0 ; i< min(first.size(),last.size());i++){
+            if( first[i] != last[i]){
+                return result;
+            }
+            result+= first[i];
+        }
+ return result;   } 
+};
